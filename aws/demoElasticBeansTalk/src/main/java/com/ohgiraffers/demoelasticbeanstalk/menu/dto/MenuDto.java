088e299946612a1ca2841424f5bc931652d8fd1c
jeonjinhyun <jjh06500@gmail.com>
2024-10-23 14:51:25 +0900

    1023

---
+++ aws/demoElasticBeansTalk/src/main/java/com/ohgiraffers/demoelasticbeanstalk/menu/dto/MenuDto.java
@@ -0,0 +1,2 @@
+package com.ohgiraffers.demoelasticbeanstalk.menu.dto;public class MenuDTO {
+}
---
+++ aws/demoElasticBeansTalk/src/main/java/com/ohgiraffers/demoelasticbeanstalk/menu/service/MenuService.java
@@ -0,0 +1,2 @@
+package com.ohgiraffers.demoelasticbeanstalk.menu.service;public class MenuService {
+}
